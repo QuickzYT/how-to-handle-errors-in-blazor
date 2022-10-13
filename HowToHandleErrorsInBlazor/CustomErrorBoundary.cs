00f3ee3f1fc48805323cdc17df436dbcb6e58b2a
Quickz <alexserg@inbox.lv>
2022-10-13 22:49:05 +0300

    added the project

---
+++ HowToHandleErrorsInBlazor/CustomErrorBoundary.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace HowToHandleErrorsInBlazor
+{
+    public class CustomErrorBoundary : ErrorBoundary
+    {
+        [Inject]
+        private IWebAssemblyHostEnvironment Environment { get; set; }
+
+        protected override Task OnErrorAsync(Exception exception)
+        {
+            if (Environment.IsDevelopment())
+                return base.OnErrorAsync(exception);
+            else
+                return Task.CompletedTask;
+        }
+    }
+}
